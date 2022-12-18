bb54904ab77c43f7a18d9472d286091a4cf18482
mugac <71031347+mugac@users.noreply.github.com>
2022-12-18 02:19:46 +0100

    first commit

---
+++ dloader.py
@@ -0,0 +1,21 @@
+from pytube import Playlist
+import os
+
+#add path
+path = ""
+
+#add playlist link (make sure to set plalist as public)
+playlist = Playlist("")
+  
+for video in playlist.videos:
+    try:
+        videodl = video.streams.filter(only_audio=True).first()
+        out_file = videodl.download(output_path=path)
+
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        print(video.title + " has been downloaded.")
+    except:
+        print("Something went wrong while downloading: "+video.title)
+
